7c8b1ccfeca6bf58fd998526bae004efc1dbd9cb
ConcoMB <cmaderblanco@gmail.com>
2014-03-30 09:46:53 -0300

    Initial Commit

---
+++ Tower Defense/Tower Defense/Tower Defense/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  Tower Defense
+//
+//  Created by Conrado MB on 3/30/14.
+//  Copyright Conrado MB 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ Tower Defense/Tower Defense/Tower Defense/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Tower Defense
+//
+//  Created by Conrado MB on 3/30/14.
+//  Copyright Conrado MB 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
